270f54297d98d15bdb6d51b62bdb982fab4ad654
Jai Rawat <68697355+jairaj0@users.noreply.github.com>
2021-07-07 23:49:57 +0530

    new commit

--- Js/main.js
+++ Js/main.js
@@ -1,2 +1,88 @@
-const usernameValue=document.querySelector("#username"),passwordValue=document.querySelector("#password"),rePasswordValue=document.querySelector("#rePassword"),re_enter=document.querySelector("#re_enter"),signUpBtn=document.querySelector("#sign_up_btn");let signInData;function checkWindow(){const e=getData("username"),n=getData("password");if(n){signIn(),document.getElementById("signInBtn").addEventListener("click",function(){usernameValue.value&&passwordValue.value&&(checkUsernamePassword(usernameValue.value,e)?checkUsernamePassword(passwordValue.value,n)?(document.querySelector("#box").style.display="none",document.querySelector("h1.title").innerText="Hey Bro What are you doing here !!!!"):console.log("wrong Password !!!"):console.log("wrong username !!!"))})}}function signUpData(e,n,s){return{username:e.value,password:n.value,rePassword:s.value}}function store(e,n){"undefined"!=typeof Storage?(localStorage.setItem("username",e),localStorage.setItem("password",n)):console.log("Sorry, your browser does not support Web Storage...")}function getData(e){return localStorage.getItem(`${e}`)}function signIn(){re_enter.style.display="none",signUpBtn.innerText="Sign In",signUpBtn.id="signInBtn",usernameValue.value="",passwordValue.value="",document.querySelector("h1.title").innerText="SIGN IN"}function checkUsernamePassword(e,n){return e===n}checkWindow(),signUpBtn.addEventListener("click",function(){const e=signUpData(usernameValue,passwordValue,rePasswordValue);usernameValue.value&&passwordValue.value&&rePasswordValue.value&&e.password===e.rePassword?(console.log("successfully sign up !"),store(e.username,e.password),signIn()):console.log("Something went wrong !!!")});
-document.onkeydown=function(e){return 123!=event.keyCode&&((!e.ctrlKey||e.keyCode!="E".charCodeAt(0))&&((!e.ctrlKey||!e.shiftKey||e.keyCode!="I".charCodeAt(0))&&((!e.ctrlKey||!e.shiftKey||e.keyCode!="J".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="U".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="S".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="H".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="A".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="F".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="E".charCodeAt(0))&&void 0)))))))))},document.addEventListener("contextmenu",e=>e.preventDefault());
+const usernameValue = document.querySelector("#username");
+const passwordValue = document.querySelector("#password");
+const rePasswordValue = document.querySelector("#rePassword");
+const re_enter = document.querySelector("#re_enter");
+const signUpBtn = document.querySelector("#sign_up_btn");
+let signInData;
+
+checkWindow()
+
+signUpBtn.addEventListener("click", function () {
+  const data = signUpData(usernameValue, passwordValue, rePasswordValue);
+
+  if (usernameValue.value && passwordValue.value && rePasswordValue.value && data.password === data.rePassword) {
+    console.log("successfully sign up !");
+    store(data.username, data.password);
+    signIn()
+  } else {
+    console.log("Something went wrong !!!");
+  }
+});
+
+function checkWindow() {
+        // get data
+        const getUsername = getData("username");
+        const getPassword = getData("password");
+        // console.log(getUsername, getPassword);
+    
+    if (getUsername , getPassword) {
+        signIn()
+        const signInBtn = document.getElementById('signInBtn');
+        signInBtn.addEventListener('click', function () {
+            if (usernameValue.value && passwordValue.value){
+                if (checkUsernamePassword(usernameValue.value , getUsername)) {
+                    if (checkUsernamePassword(passwordValue.value , getPassword)) {
+                        document.querySelector('#box').style.display = "none";
+                        document.querySelector('h1.title').innerText = 'Hey Bro What are you doing here !!!!';
+                    }else{
+                        console.log('wrong Password !!!')
+                    }
+                }else{
+                    console.log('wrong username !!!')
+                }
+            }
+        })
+    }
+}
+
+function signUpData(username, password, re_password) {
+  const mainData = {
+    username: username.value,
+    password: password.value,
+    rePassword: re_password.value,
+  };
+  return mainData;
+}
+
+function store(username, password) {
+  // Check browser support
+  if (typeof Storage !== "undefined") {
+    // Store
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+  } else {
+    console.log("Sorry, your browser does not support Web Storage...");
+  }
+}
+
+// Retrieve Data From Local Storage
+function getData(nameOfData) {
+  return localStorage.getItem(`${nameOfData}`);
+}
+
+function signIn() {
+    re_enter.style.display = "none";
+    signUpBtn.innerText = 'Sign In';
+    signUpBtn.id = "signInBtn";
+    usernameValue.value = "";
+    passwordValue.value = "";
+    document.querySelector('h1.title').innerText = 'SIGN IN';
+}
+
+function checkUsernamePassword(inputValue , getValue) {
+    if (inputValue === getValue) {
+        return true;
+    }else{
+        return false;
+    }
+}
